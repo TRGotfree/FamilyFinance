7846acfc8ee5c4f85b18e3157a7f11e234e8fb0d
Gazaleev Timur <timsmailbox@inbox.ru>
2020-04-20 22:20:47 +0500

    IRepository implemented

--- FamilyFinace/Interfaces/IRepository.cs
+++ FamilyFinace/Interfaces/IRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FamilyFinace.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,53 @@ namespace FamilyFinace.Interfaces
 {
     public interface IRepository
     {
-       
+        Task<IEnumerable<Cost>> GetCosts(DateTime date);
+        
+        Task<Cost> AddCost(Cost cost);
+        
+        Task<Cost> UpdateCost(Cost cost);
+        
+        Task DeleteCost(Cost cost);
+
+        Task<IEnumerable<CostCategory>> GetCostCategories();
+
+        Task<CostCategory> AddCostCategory(CostCategory costCategory);
+
+        Task<CostCategory> UpdateCostCategory(CostCategory costCategory);
+
+        Task DeleteCostCategory(CostCategory costCategory);
+
+        Task<IEnumerable<CostSubCategory>> GetCostSubCategories();
+
+        Task<CostSubCategory> AddCostSubCategory(CostSubCategory costSubCategory);
+
+        Task<CostSubCategory> UpdateCostSubCategory(CostSubCategory costSubCategory);
+
+        Task DeleteCostSubCategory(CostSubCategory costSubCategory);
+
+        Task<IEnumerable<Store>> GetStores();
+
+        Task<Store> AddStore(Store store);
+
+        Task<Store> UpdateStore(Store store);
+
+        Task DeleteStore(Store store);
+
+        Task<IEnumerable<PayType>> GetPayTypes();
+
+        Task<PayType> AddPayType(PayType payType);
+
+        Task<PayType> UpdatePayType(PayType payType);
+
+        Task DeletePayType(PayType payType);
+
+        Task<IEnumerable<Income>> GetIncomes();
+
+        Task<Income> AddIncome(Income income);
+
+        Task<Income> UpdateIncome(Income income);
+
+        Task DeleteIncome(Income income);
+
     }
 }
--- FamilyFinace/Services/RepositoryProvider.cs
+++ FamilyFinace/Services/RepositoryProvider.cs
@@ -1,4 +1,5 @@
 ﻿using FamilyFinace.Interfaces;
+using FamilyFinace.Models;
 using FamilyFinace.Repository;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -19,6 +20,221 @@ namespace FamilyFinace.Services
             this.repository = repositoryContext ?? throw new ArgumentNullException(nameof(repositoryContext));
         }
 
+        public async Task<Cost> AddCost(Cost cost)
+        {
+            if (cost == null)
+                throw new ArgumentNullException(nameof(cost));
 
+            await repository.AddAsync(cost);
+            await repository.SaveChangesAsync();
+
+            return cost;
+        }
+
+        public async Task<CostCategory> AddCostCategory(CostCategory costCategory)
+        {
+            if (costCategory == null)
+                throw new ArgumentNullException(nameof(costCategory));
+
+            await repository.AddAsync(costCategory);
+            await repository.SaveChangesAsync();
+
+            return costCategory;
+        }
+
+        public async Task<CostSubCategory> AddCostSubCategory(CostSubCategory costSubCategory)
+        {
+            if (costSubCategory == null)
+                throw new ArgumentNullException(nameof(costSubCategory));
+
+            await repository.AddAsync(costSubCategory);
+            await repository.SaveChangesAsync();
+
+            return costSubCategory;
+        }
+
+        public async Task<Income> AddIncome(Income income)
+        {
+            if (income == null)
+                throw new ArgumentNullException(nameof(income));
+
+            await repository.AddAsync(income);
+            await repository.SaveChangesAsync();
+
+            return income;
+        }
+
+        public async Task<PayType> AddPayType(PayType payType)
+        {
+            if (payType == null)
+                throw new ArgumentNullException(nameof(payType));
+
+            await repository.AddAsync(payType);
+            await repository.SaveChangesAsync();
+
+            return payType;
+        }
+
+        public async Task<Store> AddStore(Store store)
+        {
+            if (store == null)
+                throw new ArgumentNullException(nameof(store));
+
+            await repository.AddAsync(store);
+            await repository.SaveChangesAsync();
+
+            return store;
+        }
+
+        public async Task DeleteCost(Cost cost)
+        {
+            if (cost == null)
+                throw new ArgumentNullException(nameof(cost));
+
+            repository.Remove(cost);
+            await repository.SaveChangesAsync();
+        }
+
+        public async Task DeleteCostCategory(CostCategory costCategory)
+        {
+            if (costCategory == null)
+                throw new ArgumentNullException(nameof(costCategory));
+
+            repository.Remove(costCategory);
+            await repository.SaveChangesAsync();
+        }
+
+        public async Task DeleteCostSubCategory(CostSubCategory costSubCategory)
+        {
+            if (costSubCategory == null)
+                throw new ArgumentNullException(nameof(costSubCategory));
+
+
+            repository.Remove(costSubCategory);
+            await repository.SaveChangesAsync();
+        }
+
+        public async Task DeleteIncome(Income income)
+        {
+            if (income == null)
+                throw new ArgumentNullException(nameof(income));
+
+            repository.Remove(income);
+            await repository.SaveChangesAsync();
+        }
+
+        public async Task DeletePayType(PayType payType)
+        {
+            if (payType == null)
+                throw new ArgumentNullException(nameof(payType));
+
+            repository.Remove(payType);
+            await repository.SaveChangesAsync();
+        }
+
+        public async Task DeleteStore(Store store)
+        {
+            if (store == null)
+                throw new ArgumentNullException(nameof(store));
+
+            repository.Remove(store);
+            await repository.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<CostCategory>> GetCostCategories()
+        {
+            return await repository.CostCategory.Where(c => !c.IsRemoved).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Cost>> GetCosts(DateTime date)
+        {
+            return await repository.Cost.Where(c => c.Date == date).ToListAsync();
+        }
+
+        public async Task<IEnumerable<CostSubCategory>> GetCostSubCategories()
+        {
+            return await repository.CostSubCategory.Where(c => !c.IsRemoved).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Income>> GetIncomes()
+        {
+            return await repository.Income.ToListAsync();
+        }
+
+        public async Task<IEnumerable<PayType>> GetPayTypes()
+        {
+            return await repository.PayType.Where(pt => !pt.IsRemoved).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Store>> GetStores()
+        {
+            return await repository.Store.Where(s => !s.IsRemoved).ToListAsync();
+        }
+
+        public async Task<Cost> UpdateCost(Cost cost)
+        {
+            if (cost == null)
+                throw new ArgumentNullException(nameof(cost));
+
+            repository.Update(cost);
+            await repository.SaveChangesAsync();
+
+            return cost;
+        }
+
+        public async Task<CostCategory> UpdateCostCategory(CostCategory costCategory)
+        {
+            if (costCategory == null)
+                throw new ArgumentNullException(nameof(costCategory));
+
+            repository.Update(costCategory);
+            await repository.SaveChangesAsync();
+
+            return costCategory;
+        }
+
+        public async Task<CostSubCategory> UpdateCostSubCategory(CostSubCategory costSubCategory)
+        {
+            if (costSubCategory == null)
+                throw new ArgumentNullException(nameof(costSubCategory));
+
+            repository.Update(costSubCategory);
+            await repository.SaveChangesAsync();
+
+            return costSubCategory;
+        }
+
+        public async Task<Income> UpdateIncome(Income income)
+        {
+            if (income == null)
+                throw new ArgumentNullException(nameof(income));
+
+            repository.Update(income);
+            await repository.SaveChangesAsync();
+
+            return income;
+        }
+
+        public async Task<PayType> UpdatePayType(PayType payType)
+        {
+            if (payType == null)
+                throw new ArgumentNullException(nameof(payType));
+
+            repository.Update(payType);
+            await repository.SaveChangesAsync();
+
+            return payType;
+        }
+
+        public async Task<Store> UpdateStore(Store store)
+        {
+            if (store == null)
+                throw new ArgumentNullException(nameof(store));
+
+            repository.Update(store);
+            await repository.SaveChangesAsync();
+
+            return store;
+        }
     }
 }
